0979bbfe2d6da10e1a3c2647710f8f512eecc792
icode <icode@luoxiaojiang.com>
2022-03-01 12:12:14 +0800

    hot-fix luo2

--- src/main/java/com/luoxiaojiang/git/GitTest.java
+++ src/main/java/com/luoxiaojiang/git/GitTest.java
@@ -2,6 +2,7 @@ package com.luoxiaojiang.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello IDEA.git my two git luoxiaojiang");
+        System.out.println("hello IDEA.git my two git luoxiaojian sdg");
+        System.out.println("hello IDEA.git my two git luoxiaojian sdg");
     }
 }
