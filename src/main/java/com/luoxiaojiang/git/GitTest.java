e2dd6671e9f15b3c4ebe3e8ba2ce495056ec0bcb
icode <icode@luoxiaojiang.com>
2022-03-01 11:54:29 +0800

    luo

--- src/main/java/com/luoxiaojiang/git/GitTest.java
+++ src/main/java/com/luoxiaojiang/git/GitTest.java
@@ -2,6 +2,6 @@ package com.luoxiaojiang.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello IDEA.git");
+        System.out.println("hello IDEA.git my two git");
     }
 }
